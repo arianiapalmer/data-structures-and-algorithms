0ee92c76a5be60312744ec5a647b4a53ff044d60
arianiapalmer <arianiapalmer@gmail.com>
2022-07-17 02:47:48 -0400

    finished challenge 3

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -63,11 +63,15 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
-  // Solution code here...
+  word = word.toUpperCase(); + '!';
+  return word;
 };
 
-const speaker = (words, callback) => {
-  // Solution code here...
+const speaker = (word, callback) => {
+  let newArr = [];
+  words.forEach(word => {
+
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
